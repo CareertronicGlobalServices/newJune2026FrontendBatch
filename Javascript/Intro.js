ce0d223dfbb062703228b4a6281d6d699806e093
Sonam <sonam.panjwani29@gmail.com>
2026-08-05 17:39:33 +0530

    First JS Session

---
+++ Javascript/Intro.js
@@ -0,0 +1,26 @@
+// console.log("Mai hu external JS");
+// // variables and constants
+// // //let const var
+// // let a;
+// // console.log(a);
+// // a = 70;
+// // console.log(a);
+
+// // const b = 8;
+// // console.log(b);
+// // b = 15;
+// var b;
+// console.log(b);
+// b = 76;
+// console.log(b);
+let uname = "Kiran";
+let age = 25;
+let city = "raipur";
+
+console.log("Hello World");
+console.log(uname);
+console.log(
+  "Hi I am " + uname + " and I am " + age + " old and I live in " + city,
+);
+//String Interpolation
+console.log(`Hi I am ${uname} and I am ${age} old and I live in ${city}.`);
