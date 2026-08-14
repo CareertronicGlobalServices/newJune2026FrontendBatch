2609c6b4150d373bedbe843e10f3b927d8857514
Sonam <sonam.panjwani29@gmail.com>
2026-08-14 13:36:17 +0530

    Decision

---
+++ Javascript/DecisionMaking.js
@@ -0,0 +1,245 @@
+// // Boolean thinking
+// let varOne = 5;
+// console.log(varOne >= 8);
+
+// //2. If Statements
+
+// let age = 8;
+// if (age >= 18) {
+//   console.log(`yes this guy is Adult`);
+// }
+// //3.If--else statemnets
+// let newage = 8;
+// if (newage >= 18) {
+//   console.log(`yes this guy is Adult`);
+// } else {
+//   console.log("this guy is Minor");
+// }
+
+// //nested if else
+// //Que.
+// let SName = "Vaibhav";
+// let hindi = 75;
+// let eng = 75;
+// let math = 76;
+// let computer = 70;
+// let science = 74;
+// let sst = 70;
+
+// let total = hindi + eng + math + computer + science + sst;
+// let percentage = (total / 600) * 100;
+// let Grade = "";
+// if (percentage >= 90) {
+//   Grade = "A+";
+// } else if (percentage >= 80 && percentage < 90) {
+//   Grade = "A";
+// } else if (percentage >= 70 && percentage < 80) {
+//   Grade = "B+";
+// } else if (percentage >= 60 && percentage < 70) {
+//   Grade = "B";
+// } else {
+//   Grade = "NA";
+// }
+
+// console.log(
+//   `Dear ${SName} , You have Scored ${percentage} with grade as ${Grade} `,
+// );
+
+// // Question is
+// // Write a program to check if a person can drive or not . Need the Output as Dear Rahul, You are Above 18 SO YOU CAN DRIVE OR VOTE"
+
+// let userName = "Rahul";
+// let chckage = 25;
+// let isHavingLicenseCheck = true;
+// if (chckage >= 18) {
+//   isHavingLicense = true;
+//   console.log(`Dear ${userName}, You are Above 18 SO YOU CAN DRIVE`);
+// } else {
+//   isHavingLicenseCheck = false;
+//   console.log(`Dear ${userName}, You are below 18 SO YOU CAN NOT DRIVE`);
+// }
+
+// // if (isHavingLicenseCheck) {
+// //   console.log("Bande k pas license hai");
+// // } else {
+// //   console.log("nahi hai");
+// // }
+// //Ques -- age>18 and u should have license
+
+// let userName = "Rahul";
+// let n_age = 25;
+// let isHavingLicense = true;
+
+// if (n_age >= 18 && isHavingLicense == true) {
+//   console.log(`Dear ${userName}, You can Drive`);
+// } else {
+//   console.log(`Dear ${userName}, You cannot Drive`);
+// }
+
+// Que.1
+// Write a program to check the students eligibility for college admission
+// Rules
+// 1. 12th School percentage  60 and above
+// 2. entrace exam 70 and above
+
+// let schoolPer = 45;
+// let entraceresult = 70;
+
+// if (schoolPer >= 60 && entraceresult >= 70) {
+//   console.log("Eligible");
+// } else {
+//   console.log("NA");
+// }
+
+// Que.2
+// ATM Logic
+// let balance = 5000;
+// let withdrawal = 3000;
+
+// If withdrawal > 0
+//     If withdrawal <= balance
+//         → "Transaction Successful"
+//         → Show remaining balance
+//     Else
+//         → "Insufficient Balance"
+// Else
+//     → "Invalid Amount"
+
+// Write the complete program
+// let balance = 5000;
+// let withdrawal = -2000;
+
+// if (withdrawal > 0 ) {
+//   if (withdrawal <= balance) {
+//     let remaining = balance - withdrawal;
+//     console.log(`Transaction Succesful`);
+//     console.log(`Remaining Balance is ${remaining}`);
+//   } else {
+//     console.log(`Insufficient Balance`);
+//   }
+// } else {
+//   console.log(`Invalid Amount`);
+// // }
+// let age = 20;
+// let hasID = false;
+
+// if (age >= 18) {
+//   console.log("Age is valid");
+
+//   if (hasID) {
+//     console.log("Entry Allowed");
+//   } else {
+//     console.log("ID Required");
+//   }
+// } else {
+//   console.log("Underage");
+// }
+//Ques Cehck if a enetred no is positive negative or zero
+
+// let num = -20;
+
+// if (num == 0) {
+//   console.log("Entered Number is Zero");
+// } else if (num > 0) {
+// //   console.log("number is greater than zero :Positive");
+// // } else if (num < 0) {
+// //   console.log("number is smaller than zero :Negative");
+// // }
+
+// //Ques check which is greater of all the three numbers
+
+// let a = 6;
+// let b = 8;
+// let c = 10;
+
+// if (a > b && a > c) {
+//   console.log(`A is greater than b and c`);
+// } else if (b > a && b > c) {
+//   console.log("B is greater than a and c");
+// // } else console.log("C is greater ");
+// let x = 10;
+// let y = 20;
+
+// if (x > 5) {
+//   if (y < 15) {
+//     console.log("A");
+//   } else {
+//     console.log("B");
+//   }
+// } else if (x === 10) {
+//   console.log("C");
+// } else {
+//   console.log("D");
+// }
+
+// Discount System — Coding Question
+
+// Create a program using if-else:
+// Purchase ≥ ₹10,000 → 20% discount
+// Purchase ≥ ₹5,000 → 10% discount
+// Purchase ≥ ₹2,000 → 5% discount
+// Below ₹2,000 → No discount
+// variable :- let amount;
+//output
+// Sale : 7000
+//discount:10%
+//total payable amount = 6300/-
+
+// let amount = 17500;
+// let discountPer = 0;
+
+// if (amount >= 10000) {
+//   discountPer = 20;
+// } else if (amount >= 5000) {
+//   discountPer = 10;
+// } else if (amount >= 2000) {
+//   discountPer = 5;
+// } else {
+//   discountPer = 0;
+// }
+// let amountPayable = amount - (discountPer * amount) / 100;
+// console.log(
+//   `Amount: ${amount} DiscountPercent :${discountPer} NetPayableAmount: ${amountPayable}`,
+// );
+// let letter = "b";
+
+// if (
+//   letter === "a" ||
+//   letter === "e" ||
+//   letter === "i" ||
+//   letter === "o" ||
+//   letter === "u"
+// ) {
+//   console.log("Its a Vowel");
+// } else {
+//   console.log("Its a Consonant");
+// }
+// Q9. Login + Role Based Access
+// let isLoggedIn = true;
+// let role = "editor";
+
+// Create conditions:
+
+// Not logged in → "Please login"
+// Admin → "Full access"
+// Editor → "Can edit content"
+// User → "Read only access"
+// Any other role → "Invalid role"
+
+let isLoggedIn = false;
+let role = "user";
+
+if (isLoggedIn) {
+  if (role === "Admin") {
+    console.log("Full Access");
+  } else if (role === "Editor") {
+    console.log("Can Edit Content");
+  } else if (role === "user") {
+    console.log("Read Only ");
+    b;
+  } else {
+    console.log("InvalidROle");
+  }
+} else {
+  console.log("Please Login");
+}
--- Javascript/Operators.js
+++ Javascript/Operators.js
@@ -1,35 +1,197 @@
-//Arithmetic operators
-let a = 10;
-let b = 3;
+// //Arithmetic operators
+// let a = 10;
+// let b = 3;
 
-console.log(a + b); // 13
-console.log(a - b); // 7
-console.log(a * b); // 30
-console.log(a / b); // 3.333...
+// console.log(a + b); // 13
+// console.log(a - b); // 7
+// console.log(a * b); // 30
+// console.log(a / b); // 3.333...
 
-//modulus - it returns your remainder of a division
-console.log(a % b); // 1
-//Exponent
-//power: 10 raised to 3 = 10 * 10*10 ;
+// //modulus - it returns your remainder of a division
+// console.log(a % b); // 1
+// //Exponent
+// //power: 10 raised to 3 = 10 * 10*10 ;
 
-console.log(a ** b); // 1000
+// console.log(a ** b); // 1000
 
-//2.Assignment Operator(=)
-let c = 10;
-//3.Comparision Operators ( < ,> ,<=,>=,== ,===,!= ,!==)
-// <  Lessthan
-/* > greater than ,
-<= lessthan equal to,
->= greater than eual to ,
-== equal to ,
-=== strict equal,
-!= not equal ,
-!== strict not equal*/
+// //2.Assignment Operator(=)
+// let c = 10;
 
-console.log(5 == "5"); //true -
+// //3.Comparision Operators ( < ,> ,<=,>=,== ,===,!= ,!==)
+// // <  Lessthan
+// /* > greater than ,
+// <= lessthan equal to,
+// >= greater than eual to ,
+// == equal to ,
+// === strict equal,
+// != not equal ,
+// !== strict not equal*/
 
-console.log(5 === "5"); // false stict check with datatype
+// console.log(5 == "5"); //true -
 
-console.log(5 != "5"); //false
+// console.log(5 === "5"); // false stict check with datatype
 
-console.log(5 !== "5"); //true Strict Check
+// console.log(5 != "5"); //false
+
+// console.log(5 !== "5"); //true Strict Check
+
+//Booleans Logical operators
+
+// 1.And (&&)
+
+// both the condition should be true
+
+// let age = 18;
+// let hasLicense = true;
+
+// console.log(age > 18 && hasLicense);
+
+//2.OR(||)
+
+// let age = 18;
+// let hasLicense = true;
+
+// console.log(age > 18 || hasLicense);
+
+// //Not
+// console.log(!hasLicense);
+
+//Increment(++) and decrement(--) operators
+
+// let count = 5;
+// console.log(count);
+// count++;
+// console.log(`Increment ${count}`);
+
+// count--;
+// console.log(`Decrement ${count}`);
+
+//post INC/DEC operator
+
+//let a = 6;
+
+//console.log(a++ + "a dekhte h " + a);
+// console.log(a);
+
+// let b = 25;
+// console.log(b--);
+// console.log(b);
+
+//pre INC/DEC
+
+// let a = 6;
+
+// // console.log(++a);
+// // console.log(a);
+
+// // let b = 25;
+// // console.log(--b);
+// // console.log(b);
+
+// //Question
+// // let x = 5;
+// // let y = 10;
+// // console.log(x++ + ++x);
+// // //console.log(x++ + y++);
+
+// let a = 10;
+
+// console.log(++a + a++);
+// console.log(a);
+
+// //pre = ++ pehle ++x , incremented value hi  le lo
+
+// //post = ++ baad me x++ , tum abhi current value use karlo next se incremented value lena
+// let m = 4;
+// console.log(m++ + ++m + m++ + ++m + m++ + m++);
+// //4 + 6 + 6 +8+8+9
+
+// let x = 5;
+
+// console.log(x++ + x++);
+// //5 + 6 =11
+
+// console.log(x);
+// //7
+// ``````js
+// let x = 5;
+
+// console.log(++x + ++x);
+// 6 + 7;
+// console.log(x);
+// 7;
+
+// // ``````js
+// let x = 5;
+
+// let y = x++ + ++x;
+// //5 + 7
+// console.log(y); //12
+// console.log(x); //7
+// ``````js
+// let x = 10;
+
+// let y = x-- + --x;
+// 10 + 8;
+
+// console.log(y);
+// 18;
+// console.log(x);
+// 8;
+
+// // ``````js
+// let x = 3;
+
+// let y = ++x + x++ + ++x;
+// // 4 + 4 + 6;
+// // console.log(y);
+// // 14;
+// // console.log(x);
+// // 6;
+// // ``` -
+// // //   ```js
+// let x = 5;
+
+// let y = x++ + x++ + ++x;
+// //5+6+8
+// console.log(y); //19
+// console.log(x); //8
+
+// // ``````js
+// let x = 5;
+
+// let y = x++ + ++x + x++ + ++x;
+// //5+7+7+9 =28
+// console.log(y);
+// 28;
+// console.log(x);
+// 9;
+// // // ```;
+// let x = 5;
+// console.log(x++ + ++x - x--);
+// //5 + 7 - 7;
+// console.log(x); //6
+
+// let a = 5;
+// let b = a++ + ++a;
+// let c = --a + a--;
+// let d = ++a + a++;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+//Ternary Operator(condition ? trueOption : falseOption)
+
+//short form If-else
+
+let age = 2;
+let check = age > 18 ? "ADULT" : "MINOR";
+console.log(check);
+
+//Even /odd
+
+let num = 6;
+
+let check2 = num % 2 == 0 ? "Even" : "Odd";
+console.log(check2);
